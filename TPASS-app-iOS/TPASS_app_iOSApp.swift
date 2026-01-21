26f37dee3a1889ea497b9ba4270569a2d3dbbe4d
Ray Hsu (MacBook Air M3) <s111319022@stu.ntue.edu.tw>
2026-01-21 15:47:36 +0800

    Initial Commit

---
+++ TPASS-app-iOS/TPASS_app_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TPASS_app_iOSApp.swift
+//  TPASS-app-iOS
+//
+//  Created by Ray Hsu on 2026/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TPASS_app_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
